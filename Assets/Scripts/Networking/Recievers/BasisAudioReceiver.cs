c73c84ff0034483391108ce1c3d4b725cffd1fe0
dooly <doolanl208@gmail.com>
2024-07-19 10:35:00 +1000

    cleaned up

--- Assets/Scripts/Networking/Recievers/BasisAudioReceiver.cs
+++ Assets/Scripts/Networking/Recievers/BasisAudioReceiver.cs
@@ -1,86 +1,9 @@
-<<<<<<< Updated upstream
-﻿using System;
-using System.Collections;
-=======
-﻿#if UNITY_EDITOR
 using UnityEditor;
-#endif
->>>>>>> Stashed changes
 using UnityEngine;
 
 [System.Serializable]
 public class BasisAudioReceiver : BasisAudioReceiverBase
 {
-<<<<<<< Updated upstream
-    [SerializeField]
-    public BasisAudioDecoder decoder;
-    [SerializeField]
-    public AudioSource audioSource;
-    [SerializeField]
-    public BasisVisemeDriver visemeDriver;
-    [SerializeField]
-    public BasisOpusSettings settings;
-    public int samplingFrequency;
-    public int numChannels;
-    public int SampleLength;
-    public float[] ringBuffer;
-    public int head = 0;
-    public float lastDataReceivedTime;
-    public float dataTimeout = 0.1f; // Timeout period in seconds
-    public float[] SilentData;
-    public int DataSize;
-    public bool HasEvents = false;
-    public void LateUpdate()
-    {
-        if (Time.time - lastDataReceivedTime > dataTimeout)
-        {
-            if (audioSource.isPlaying)
-            {
-                ClearRingBuffer();
-            }
-        }
-    }
-
-    public void ClearRingBuffer()
-    {
-        Array.Clear(ringBuffer, 0, SampleLength);
-        head = 0;
-    }
-
-    public void OnDecoded()
-    {
-        OnDecoded(decoder.pcmBuffer, decoder.pcmLength);
-    }
-
-    public void OnDecoded(float[] pcm, int pcmLength)
-    {
-        // Write incoming PCM data into the ring buffer
-        for (int index = 0; index < pcmLength; index++)
-        {
-            ringBuffer[(head + index) % SampleLength] = pcm[index];
-        }
-
-        head = (head + pcmLength) % SampleLength;
-
-        // Update the AudioClip with the new data
-        audioSource.clip.SetData(ringBuffer, 0);
-
-        // Do we have enough data
-        if (!audioSource.isPlaying && head > SampleLength / 2)
-        {
-            audioSource.Play();
-        }
-
-        // Update the timestamp of the last received data
-        lastDataReceivedTime = Time.time;
-    }
-
-    public void OnDecodedSilence(float[] pcm, int pcmLength)
-    {
-        OnDecoded(pcm, pcmLength);
-    }
-
-=======
 #if UNITY_EDITOR
     [MenuItem("Basis/Playback/Test")]
     public static void PlayBackThisAudio()
@@ -88,10 +11,9 @@ public class BasisAudioReceiver : BasisAudioReceiverBase
         BasisAudioReceiver Rec = GameObject.FindFirstObjectByType<BasisNetworkReceiver>().AudioReceiverModule;
         Rec.PlayEntireBuffer();
     }
-#endif
->>>>>>> Stashed changes
     public void OnEnable(BasisNetworkedPlayer networkedPlayer, GameObject audioParent)
     {
+        // Initialize settings and audio source
         settings = BasisDeviceManagement.Instance.BasisOpusSettings;
         if (audioSource == null)
         {
@@ -99,45 +21,44 @@ public class BasisAudioReceiver : BasisAudioReceiverBase
             audioSource = BasisHelpers.GetOrAddComponent<AudioSource>(remotePlayer.AudioSourceGameobject);
             audioSource.spatialize = true;
             audioSource.spatializePostEffects = false;
-            audioSource.spatialBlend = 1.0f; // Ensure audio is fully 3D spatialized
+            audioSource.spatialBlend = 1.0f; // Fully 3D spatialized
         }
+
+        // Initialize sampling parameters
         samplingFrequency = settings.GetSampleFreq();
         numChannels = settings.GetChannelAsInt();
         SampleLength = samplingFrequency * numChannels;
-<<<<<<< Updated upstream
-        ringBuffer = new float[SampleLength];
-        audioSource.clip = AudioClip.Create($"player [{networkedPlayer.NetId}]", SampleLength, numChannels, samplingFrequency, false);
-=======
 
         // Create AudioClip
         Buffer = new CircularBuffer(SegmentSize, MaximumStored);
         audioSource.clip = AudioClip.Create($"player [{networkedPlayer.NetId}]", Buffer.BufferSize, numChannels, samplingFrequency, false);
         // Ensure decoder is initialized and subscribe to events
->>>>>>> Stashed changes
         if (decoder == null)
         {
             decoder = BasisHelpers.GetOrAddComponent<BasisAudioDecoder>(audioParent);
         }
-        if (HasEvents == false)
-        {
-            decoder.OnDecoded += OnDecoded;
-            HasEvents = true;
-        }
+        decoder.OnDecoded += OnDecoded;
+
+        // Start audio playback
         audioSource.loop = false;
         audioSource.Play();
+
+        // Perform calibration
         OnCalibration(networkedPlayer);
     }
+
     public void OnDestroy()
     {
-        if (HasEvents)
+        // Unsubscribe from events on destroy
+        if (decoder != null)
         {
             decoder.OnDecoded -= OnDecoded;
-            HasEvents = false;
         }
     }
 
     public void OnDisable()
     {
+        // Clean up audio and related components
         if (decoder != null)
         {
             decoder.OnDecoded -= OnDecoded;
@@ -156,12 +77,10 @@ public class BasisAudioReceiver : BasisAudioReceiverBase
         }
     }
 
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
     public void OnCalibration(BasisNetworkedPlayer networkedPlayer)
     {
+        // Ensure viseme driver is initialized for audio processing
         if (visemeDriver == null)
         {
             visemeDriver = BasisHelpers.GetOrAddComponent<BasisVisemeDriver>(audioSource.gameObject);
