b36d4a08137e76d213e635f95d312d2077375a0a
dooly <doolanl208@gmail.com>
2024-09-06 12:01:25 +1000

    removed lerp

--- Packages/JigglePhysics/Scripts/JiggleSettingData.cs
+++ Packages/JigglePhysics/Scripts/JiggleSettingData.cs
@@ -12,19 +12,5 @@ namespace JigglePhysics
         public float lengthElasticity;
         public float elasticitySoften;
         public float radiusMultiplier;
-        public static JiggleSettingsData Lerp(JiggleSettingsData a, JiggleSettingsData b, float t)
-        {
-            return new JiggleSettingsData
-            {
-                gravityMultiplier = Mathf.Lerp(a.gravityMultiplier, b.gravityMultiplier, t),
-                friction = Mathf.Lerp(a.friction, b.friction, t),
-                angleElasticity = Mathf.Lerp(a.angleElasticity, b.angleElasticity, t),
-                blend = Mathf.Lerp(a.blend, b.blend, t),
-                airDrag = Mathf.Lerp(a.airDrag, b.airDrag, t),
-                lengthElasticity = Mathf.Lerp(a.lengthElasticity, b.lengthElasticity, t),
-                elasticitySoften = Mathf.Lerp(a.elasticitySoften, b.elasticitySoften, t),
-                radiusMultiplier = Mathf.Lerp(a.radiusMultiplier, b.radiusMultiplier, t),
-            };
-        }
     }
 }
