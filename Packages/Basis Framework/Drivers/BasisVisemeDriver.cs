2d30d82badbc1733a8f26df65a5d55220647fd39
dooly <doolanl208@gmail.com>
2024-09-09 00:56:19 +1000

    another check for viseme compatability

--- Packages/Basis Framework/Drivers/BasisVisemeDriver.cs
+++ Packages/Basis Framework/Drivers/BasisVisemeDriver.cs
@@ -30,113 +30,115 @@ namespace Basis.Scripts.Drivers
         {
             Avatar = BasisPlayer.Avatar;
             Player = BasisPlayer;
-            if (Avatar.FaceVisemeMesh != null)
+            if (Avatar == null)
             {
-                // Start loading the ScriptableObject from Addressables using the addressable key
-                AsyncOperationHandle<uLipSync.Profile> handle = Addressables.LoadAssetAsync<uLipSync.Profile>("Packages/com.hecomi.ulipsync/Assets/Profiles/uLipSync-Profile-Sample.asset");
-
-                // Wait for the operation to complete
-                handle.WaitForCompletion();
-                //Packages/com.hecomi.ulipsync/Assets/Profiles/uLipSync-Profile-Sample.asset
-                profile = handle.Result;
-
-                uLipSync = this.gameObject.AddComponent<uLipSync.uLipSync>();
-                uLipSync.profile = profile;
-
-                uLipSyncBlendShape = this.gameObject.AddComponent<uLipSyncBlendShape>();
-                uLipSyncBlendShape.skinnedMeshRenderer = Avatar.FaceVisemeMesh;
-                uLipSyncBlendShape.updateMethod = UpdateMethod.External;
-
-                BlendShapeCount = Avatar.FaceVisemeMovement.Length;
-                HasViseme = new bool[BlendShapeCount];
-                for (int Index = 0; Index < BlendShapeCount; Index++)
-                {
-                    if (Avatar.FaceVisemeMovement[Index] != -1)
-                    {
-                        HasViseme[Index] = true;
-                        switch (Index)
-                        {
-                            case 10:
-                                {
-                                    PhonemeBlendShapeInfo PhonemeBlendShapeInfo = new PhonemeBlendShapeInfo
-                                    {
-                                        phoneme = "A",
-                                        blendShape = Index
-                                    };
-                                    phonemeBlendShapeTable.Add(PhonemeBlendShapeInfo);
-                                    break;
-                                }
-
-                            case 12:
-                                {
-                                    PhonemeBlendShapeInfo PhonemeBlendShapeInfo = new PhonemeBlendShapeInfo
-                                    {
-                                        phoneme = "I",
-                                        blendShape = Index
-                                    };
-                                    phonemeBlendShapeTable.Add(PhonemeBlendShapeInfo);
-                                    break;
-                                }
-
-                            case 14:
-                                {
-                                    PhonemeBlendShapeInfo PhonemeBlendShapeInfo = new PhonemeBlendShapeInfo
-                                    {
-                                        phoneme = "U",
-                                        blendShape = Index
-                                    };
-                                    phonemeBlendShapeTable.Add(PhonemeBlendShapeInfo);
-                                    break;
-                                }
-
-                            case 11:
-                                {
-                                    PhonemeBlendShapeInfo PhonemeBlendShapeInfo = new PhonemeBlendShapeInfo
-                                    {
-                                        phoneme = "E",
-                                        blendShape = Index
-                                    };
-                                    phonemeBlendShapeTable.Add(PhonemeBlendShapeInfo);
-                                    break;
-                                }
-                            case 13:
-                                {
-                                    PhonemeBlendShapeInfo PhonemeBlendShapeInfo = new PhonemeBlendShapeInfo
-                                    {
-                                        phoneme = "O",
-                                        blendShape = Index
-                                    };
-                                    phonemeBlendShapeTable.Add(PhonemeBlendShapeInfo);
-                                    break;
-                                }
-                            case 7:
-                                {
-                                    PhonemeBlendShapeInfo PhonemeBlendShapeInfo = new PhonemeBlendShapeInfo
-                                    {
-                                        phoneme = "S",
-                                        blendShape = Index
-                                    };
-                                    phonemeBlendShapeTable.Add(PhonemeBlendShapeInfo);
-                                    break;
-                                }
-
-                        }
-                    }
-                    else
-                    {
-                        HasViseme[Index] = false;
-                    }
-                }
-
-
-                for (int Index = 0; Index < phonemeBlendShapeTable.Count; Index++)
-                {
-                    PhonemeBlendShapeInfo info = phonemeBlendShapeTable[Index];
-                    uLipSyncBlendShape.AddBlendShape(info.phoneme, info.blendShape);
-                }
-                uLipSyncBlendShape.updateMethod = UpdateMethod.LipSyncUpdateEvent;
-                uLipSync.onLipSyncUpdate.AddListener(uLipSyncBlendShape.OnLipSyncUpdate);
+                return;
             }
+            if (Avatar.FaceVisemeMesh == null)
+            {
+                return;
+            }
+            // Start loading the ScriptableObject from Addressables using the addressable key
+            AsyncOperationHandle<uLipSync.Profile> handle = Addressables.LoadAssetAsync<uLipSync.Profile>("Packages/com.hecomi.ulipsync/Assets/Profiles/uLipSync-Profile-Sample.asset");
+
+            // Wait for the operation to complete
+            handle.WaitForCompletion();
+            profile = handle.Result;
+
+            uLipSync = this.gameObject.AddComponent<uLipSync.uLipSync>();
+            uLipSync.profile = profile;
+
+            uLipSyncBlendShape = this.gameObject.AddComponent<uLipSyncBlendShape>();
+            uLipSyncBlendShape.skinnedMeshRenderer = Avatar.FaceVisemeMesh;
+            uLipSyncBlendShape.updateMethod = UpdateMethod.External;
+
+            BlendShapeCount = Avatar.FaceVisemeMovement.Length;
+            HasViseme = new bool[BlendShapeCount];
+            for (int Index = 0; Index < BlendShapeCount; Index++)
+            {
+                if (Avatar.FaceVisemeMovement[Index] != -1)
+                {
+                    HasViseme[Index] = true;
+                    switch (Index)
+                    {
+                        case 10:
+                            {
+                                PhonemeBlendShapeInfo PhonemeBlendShapeInfo = new PhonemeBlendShapeInfo
+                                {
+                                    phoneme = "A",
+                                    blendShape = Index
+                                };
+                                phonemeBlendShapeTable.Add(PhonemeBlendShapeInfo);
+                                break;
+                            }
+
+                        case 12:
+                            {
+                                PhonemeBlendShapeInfo PhonemeBlendShapeInfo = new PhonemeBlendShapeInfo
+                                {
+                                    phoneme = "I",
+                                    blendShape = Index
+                                };
+                                phonemeBlendShapeTable.Add(PhonemeBlendShapeInfo);
+                                break;
+                            }
+
+                        case 14:
+                            {
+                                PhonemeBlendShapeInfo PhonemeBlendShapeInfo = new PhonemeBlendShapeInfo
+                                {
+                                    phoneme = "U",
+                                    blendShape = Index
+                                };
+                                phonemeBlendShapeTable.Add(PhonemeBlendShapeInfo);
+                                break;
+                            }
+
+                        case 11:
+                            {
+                                PhonemeBlendShapeInfo PhonemeBlendShapeInfo = new PhonemeBlendShapeInfo
+                                {
+                                    phoneme = "E",
+                                    blendShape = Index
+                                };
+                                phonemeBlendShapeTable.Add(PhonemeBlendShapeInfo);
+                                break;
+                            }
+                        case 13:
+                            {
+                                PhonemeBlendShapeInfo PhonemeBlendShapeInfo = new PhonemeBlendShapeInfo
+                                {
+                                    phoneme = "O",
+                                    blendShape = Index
+                                };
+                                phonemeBlendShapeTable.Add(PhonemeBlendShapeInfo);
+                                break;
+                            }
+                        case 7:
+                            {
+                                PhonemeBlendShapeInfo PhonemeBlendShapeInfo = new PhonemeBlendShapeInfo
+                                {
+                                    phoneme = "S",
+                                    blendShape = Index
+                                };
+                                phonemeBlendShapeTable.Add(PhonemeBlendShapeInfo);
+                                break;
+                            }
+
+                    }
+                }
+                else
+                {
+                    HasViseme[Index] = false;
+                }
+            }
+            for (int Index = 0; Index < phonemeBlendShapeTable.Count; Index++)
+            {
+                PhonemeBlendShapeInfo info = phonemeBlendShapeTable[Index];
+                uLipSyncBlendShape.AddBlendShape(info.phoneme, info.blendShape);
+            }
+            uLipSyncBlendShape.updateMethod = UpdateMethod.LipSyncUpdateEvent;
+            uLipSync.onLipSyncUpdate.AddListener(uLipSyncBlendShape.OnLipSyncUpdate);
         }
         public void ProcessAudioSamples(float[] data)
         {
