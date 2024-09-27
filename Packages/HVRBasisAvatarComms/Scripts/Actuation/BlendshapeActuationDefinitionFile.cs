b46a214af8824c03098a63b3960364ee5e7de5fc
Haï~ <hai-vr@users.noreply.github.com>
2024-09-27 09:47:39 +0200

    Add networking facilities to support Face Tracking: - Add the necessary facilities to support Face Tracking, along with other systems that could be used at a later date for the networking of other non-Face Tracking related avatar features. - Add Streamed Avatar Features:   - Networks avatar features which are represented as a continuous data stream.   - All streamed features are exposed as floats from 0.0 to 1.0.   - Packets contain the deltaTime since the last packet was sent, for interpolation purposes.   - Every float is encoded within a full byte.   - Receiver linearly interpolates through received packets by unrolling the tape.   - Receiver may try to catch up when the queue duration or length gets larger. - Add Avatar Communication:   - Manages the initialization and reception of networked packets. - Add Feature Networking:   - Tracks which components in the avatar need to be networked through a GUID.   - The wearer will communicate the index of the GUID to non-wearers.   - Routes networked packets to the receivers, and provisions instances of Streamed Avatar Features to Actuators that need interpolation. - Add Blendshape Actuation:   - Blendshape Actuation Definition File specifies which addresses actuate which blendshapes, and how.   - Blendshape Actuation component specifies which SkinnedMeshRenderers will be bound by that definition file.   - At runtime, it computes which blendshapes actually exist in the SkinnedMeshRenderers.   - Only the addresses associated with blendshapes that actually exist at runtime will be registered for networking.   - All of those addresses registered for networking will be sent as a single Streamed Avatar Feature. - Add Unified Expressions Blendshape Actuation Definition File:   - Add a Blendshape Definition File, based on the Unified Blendshapes naming convention, as defined in the VRCFaceTracking document:     - https://docs.vrcft.io/docs/tutorial-avatars/tutorial-avatars-extras/unified-blendshapes - Add Eye Tracking Bone Actuation:   - Overrides the default behaviour of the eye driver, in order to rotate the eye bones based on eye tracking addresses.   - No Streamed Avatar Feature is used to network this Actuator, as we rely on Basis' default bone networking of the eye bones. - Add Acquisition Service:   - Actuators declare which addresses they want to listen to on the Acquisition Service.   - Acquisition components will submit messages to the Acquisition Service to be dispatched to the relevant Actuators. - Add OSC Acquisition Server:   - When networking is ready:     - Spawns an OSC Acquisition Server if one doesn't already exist, on port 9000,     - and Sends a "wake up" message to port 9001.   - When OSC messages are received on port 9000, and the message has a float value, it submits that message to the Acquisition Service.   - Includes Lyuma's OSC Client. - Add Acquisition Assist:   - Creates an inspector out of a Blendshape Definition File.   - When values are changed in that inspector, it submits that change to the Acquisition Service.

---
+++ Packages/HVRBasisAvatarComms/Scripts/Actuation/BlendshapeActuationDefinitionFile.cs
@@ -0,0 +1,9 @@
+using HVR.Basis.Comms;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BlendshapeActuationAsset", menuName = "HVR.Basis/Comms/Blendshape Actuation Definition FIle")]
+public class BlendshapeActuationDefinitionFile : ScriptableObject
+{
+    [TextArea] public string comment; 
+    public BlendshapeActuationDefinition[] definitions;
+}
