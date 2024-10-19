c7f4eb8a8f9db82db29ab21c52679252f82f3513
dooly <doolanl208@gmail.com>
2024-10-20 06:29:57 +1000

    fixed hais editor script

--- Packages/HVRBasisAvatarComms/Scripts/Networking/StreamedAvatarFeatureAssist.cs
+++ Packages/HVRBasisAvatarComms/Scripts/Networking/StreamedAvatarFeatureAssist.cs
@@ -1,7 +1,4 @@
-﻿using System.Linq;
-using UnityEditor;
-using UnityEngine;
-
+﻿using UnityEngine;
 namespace HVR.Basis.Comms
 {
     [AddComponentMenu("HVR.Basis/Comms/Assist/Streamed Avatar Feature Assist")]
@@ -9,41 +6,5 @@ namespace HVR.Basis.Comms
     {
         public float deltaTime = 0.1f;
         public StreamedAvatarFeature[] features;
-    }
-
-    [CustomEditor(typeof(StreamedAvatarFeatureAssist))]
-    public class StreamedAvatarFeatureAssistEditor : Editor
-    {
-        public override void OnInspectorGUI()
-        {
-            DrawDefaultInspector();
-
-            if (GUILayout.Button("Submit random"))
-            {
-                SubmitOne();
-            }
-            if (GUILayout.Button("Submit random * 10"))
-            {
-                for (var i = 0; i < 10; i++)
-                {
-                    SubmitOne();
-                }
-            }
-        }
-
-        private void SubmitOne()
-        {
-            var assist = ((StreamedAvatarFeatureAssist)target);
-            foreach (var feature in assist.features)
-            {
-                feature.QueueEvent(new StreamedAvatarFeaturePayload
-                {
-                    DeltaTime = assist.deltaTime,
-                    FloatValues = Enumerable.Range(0, feature.valueArraySize)
-                        .Select(_ => Random.Range(0f, 1f))
-                        .ToArray()
-                });
-            }
-        }
     }
 }
