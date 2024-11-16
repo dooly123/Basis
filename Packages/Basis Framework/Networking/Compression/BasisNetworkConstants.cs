422477e8dd2b4fa1f2b60ae9f9615ee1d93a5568
dooly <doolanl208@gmail.com>
2024-11-16 18:04:58 +1000

    network breaking change muscle precision

--- Packages/Basis Framework/Networking/Compression/BasisNetworkConstants.cs
+++ Packages/Basis Framework/Networking/Compression/BasisNetworkConstants.cs
@@ -2,7 +2,7 @@ namespace Basis.Scripts.Networking.Compression
 {
 public static class BasisNetworkConstants
 {
-    public static float MusclePrecision = 0.005f;
+    public static float MusclePrecision = 0.0028f;
     public static float PositionPrecision = 0.01f;
 
     //in the future I want to center min max around the starting position in the world
