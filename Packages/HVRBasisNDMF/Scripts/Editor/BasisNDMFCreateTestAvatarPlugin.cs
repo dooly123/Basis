031176341378762551b0f58b141c432be0f53422
Haï~ <hai-vr@users.noreply.github.com>
2024-12-11 22:20:30 +0100

    Make NDMF create an Addressable prefab when entering Play Mode: - If we use NDMF, entering Play Mode is not sufficient to equip the avatar for local testing. - We need to create an Addressable prefab out of the avatar, but only after the avatar has been serialized by NDMF, otherwise we'll get missing references. - NDMF serializes the avatar only when the build context "finishes", which is after all NDMF passes are completed. - To hack around this, create a NDMF plugin that runs at the end of the Optimizing phase, which will delay an invocation later which will turn the processed and serialized avatar into an Addressable prefab. - Only one avatar per scene is supported.

---
+++ Packages/HVRBasisNDMF/Scripts/Editor/BasisNDMFCreateTestAvatarPlugin.cs
@@ -0,0 +1,85 @@
+﻿#if BASISNDMF_NDMF_IS_INSTALLED
+using Basis.Scripts.BasisSdk;
+using HVR.Basis.NDMF;
+using nadena.dev.ndmf;
+using UnityEditor;
+using UnityEditor.AddressableAssets;
+using UnityEngine;
+
+[assembly: ExportsPlugin(typeof(BasisNDMFCreateTestAvatarPlugin))]
+namespace HVR.Basis.NDMF
+{
+    public class BasisNDMFCreateTestAvatarPlugin : Plugin<BasisNDMFCreateTestAvatarPlugin>
+    {
+        private const string NameOfAddressableForTestAvatar = "TestLocalAvatar";
+        private const string TestLocalAvatarPrefabLocation = "Assets/TestLocalAvatar.prefab";
+        public override string QualifiedName => "HVR.Basis.NDMF.BasisNDMFCreateTestAvatarPlugin";
+
+        protected override void Configure()
+        {
+            InPhase(BuildPhase.Optimizing)
+                .AfterPlugin("nadena.dev.modular-avatar")
+                .AfterPlugin("nadena.dev.ndmf.InternalPasses")
+                .Run("Basis NDMF: Make Addressable if Play Mode", MakeAddressableIfPlayMode);
+        }
+
+        private void MakeAddressableIfPlayMode(BuildContext context)
+        {
+            if (!Application.isPlaying) return;
+            if (!context.AvatarRootObject.GetComponent<BasisAvatar>()) return;
+
+            var asset = AssetDatabase.LoadAssetAtPath<Object>(TestLocalAvatarPrefabLocation);
+            if (asset)
+            {
+                Object.Destroy(asset);
+            }
+
+            var completedAvatar = context.AvatarRootObject;
+            
+            // We need to create the addressable prefab only after the NDMF BuildContext finishes,
+            // because the prefab needs to reference serialized assets.
+            // NDMF does not support post-processors that run after the serialization of the avatar,
+            // so we're delaying the call as a hack.
+            EditorApplication.delayCall += () =>
+            {
+                if (!Application.isPlaying) return;
+                
+                StoreAvatarAsAddressable(completedAvatar);
+            };
+        }
+
+        private static void StoreAvatarAsAddressable(GameObject completedAvatar)
+        {
+            // We don't want the AvatarActivator to trigger again when copying the avatar.
+            completedAvatar.SetActive(false);
+            var copy = Object.Instantiate(completedAvatar);
+            
+            try
+            {
+                var monoBehaviours = copy.GetComponents<MonoBehaviour>();
+                foreach (var monoBehaviour in monoBehaviours)
+                {
+                    if (monoBehaviour && monoBehaviour.GetType().FullName == "nadena.dev.ndmf.runtime.AvatarActivator")
+                    {
+                        Object.Destroy(monoBehaviour);
+                    }
+                }
+
+                var prefab = PrefabUtility.SaveAsPrefabAsset(copy, TestLocalAvatarPrefabLocation);
+                
+                var addressableSettings = AddressableAssetSettingsDefaultObject.Settings;
+                var guid = AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(prefab));
+                addressableSettings.CreateAssetReference(guid);
+                var addressableAsset = addressableSettings.FindAssetEntry(guid);
+                addressableAsset.address = NameOfAddressableForTestAvatar;
+            }
+            finally
+            {
+                Object.Destroy(copy);
+            }
+            
+            completedAvatar.SetActive(true);
+        }
+    }
+}
+#endif
