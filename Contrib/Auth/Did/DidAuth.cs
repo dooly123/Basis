0a05badaf3dc801fc1a35f64c0c6cc132fa80445
Ryan Butler <thebutlah@gmail.com>
2024-12-25 23:32:32 -0500

    refactor: create contrib dir

---
+++ Contrib/Auth/Did/DidAuth.cs
@@ -0,0 +1,8 @@
+namespace Basis.Contrib.Auth.Did
+{
+    // TODO: Create and implement an `IChallengeResponseAuth` interface. This interface should live in basis core.
+    public class DidAuthentication
+    {
+
+    }
+}
