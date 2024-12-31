e7f5678841b18a63b875a9aa0fc87235ebe69b8c
dooly <doolanl208@gmail.com>
2024-12-31 13:59:32 +1000

    moved more common files out

--- Basis/Packages/com.basis.console/BasisStaticLogInitializer.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-public static class BasisStaticLogInitializer
-{
-    [RuntimeInitializeOnLoadMethod]
-    private static void OnRuntimeMethodLoad()
-    {
-        Application.logMessageReceivedThreaded += BasisLogManager.HandleLog;
-    }
-}
